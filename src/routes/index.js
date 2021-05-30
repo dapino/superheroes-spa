86a078e99fde10a044f2d5149bad68c7bf7ad621
Daniel Pino <hola@dapino.co>
2021-05-29 21:06:34 -0500

    Create characters template

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 import Header from '../components/Header.js';
 import Home from '../views/Home.js';
 import Character from '../views/Character.js';
-
+import Error404 from '../views/Error404.js';
 const routes = {
   '/': Home,
   '/:id': Character,
---
+++ src/views/Characters.js
@@ -0,0 +1,17 @@
+const Characters = () => {
+  const view = `
+    <div class="characters">
+      <article class="character-card">
+        <img src="image" alt="name">
+      </article>
+      <article class="character-card">
+        <h3>Description</h3>
+        <h3>comics</h3>
+        <h3>Series</h3>
+      </article>
+    </div>
+  `;
+  return view;
+};
+
+export default Characters;
