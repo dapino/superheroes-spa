bc874f26d727b21774c73d21c570e7e078d5331f
dapino <hola@dapino.co>
2021-05-30 14:32:24 -0500

    Implements template for characters from data

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,21 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+  const characters = await getData();
+  console.log('characters: ', characters.data.results);
   const view = `
     <div class="characters">
-      <article class="character">
-        <a href="#/1">
-          <img src="image" alt="name">
-          <h2>Name</h2>
-        </a>
-      </article>
+      ${characters.data.results.map(
+        (character) => `
+        <a href='#/${character.id}'></a>
+        <article class="character">
+          <a href="#/1">
+            <img src="${character.thumbnail.path}/portrait_uncanny.${character.thumbnail.extension}" alt="name">
+            <h2>${character.name}</h2>
+          </a>
+        </article>
+      `
+      )}
     </div>
   `;
   return view;
