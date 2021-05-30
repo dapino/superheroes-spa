a7d2ba57e0fc39c4059b415773c0879acc5b8766
dapino <hola@dapino.co>
2021-05-30 09:11:47 -0500

    Implements Router logic

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 import Header from '../components/Header.js';
 import Home from '../views/Home.js';
-import Character from '../views/Character.js';
+import Characters from '../views/Characters.js';
 import Error404 from '../views/Error404.js';
+import getHash from '../utils/getHash.js';
+import resolveRoutes from '../utils/resolveRoutes';
+
 const routes = {
   '/': Home,
-  '/:id': Character,
+  '/:id': Characters,
   '/about': 'about',
 };
 
@@ -13,6 +16,11 @@ const router = async () => {
   const content = null || document.getElementById('content');
 
   header.innerHTML = await Header();
+
+  let hash = getHash();
+  let route = await resolveRoutes(hash);
+  let render = routes[route] || Error404;
+  content.innerHTML = await render();
 };
 
 export default router;
