ca3fd954aa5254b24c3f2e2ea2a3cf3be3f4687d
dapino <hola@dapino.co>
2021-06-01 18:50:32 -0500

    Update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 const Header = () => {
   const view = `
     <header class="navbar">
-      <div class="navbar-logo">
+      <div class="navbar__logo">
         <h1>
           <a href="/">Marvel</a>
         </h1>
       </div>
-      <nav>
+      <nav class="navbar__nav">
+        <a href="#/characters/">Characters</a>
         <a href="#/about/">About</a>
       </nav>
     </header>
